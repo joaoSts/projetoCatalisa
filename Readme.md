16ff32246f5967221cd918c7d931ce70aeb8c114
unknown <joaosantos.js234@gmail.com>
2022-01-15 13:54:12 -0300

    ajustes no index.html

--- Readme.md
+++ Readme.md
@@ -1,1 +1,8 @@
-meu projeto
+#nome do projeto:
+Rick and morty-api
+
+#descrição:
+    Como pedido foi adicionado os 3 personagens de forma aleatória
+com imagem com alt, nome, espécie e status. Como são apenas 3 personagem optei por não usar funções mais avançadas do javascript
+achei interessante adicionar um faicon(icone) antes do title e mais alguns
+detalhes na página
--- script.js
+++ script.js
@@ -3,11 +3,20 @@ const primeiroP = document.querySelector("#primeiroPersonagem");
 const segundoP = document.querySelector("#segundoPersonagem");
 const terceiroP = document.querySelector("#terceiroPersonagem");
 
+traduzirCondicao = (data) => {
+    if(data.status == 'unknown'){
+        return 'Não sabemos';
+    }else if(data.status == 'Alive'){
+        return 'Vivo';
+    }else {
+        return 'Morto';
+    }
+}
 gerarValorAleatorio = () => {
   return Math.floor(Math.random() * 826);
 };
 
-function primeiroPersonagem() {
+primeiroPersonagem=()=> {
   let numeroAleatorio = gerarValorAleatorio();
   return fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorio}`, {
     method: "GET",
@@ -22,7 +31,7 @@ function primeiroPersonagem() {
       <ul>
       <li><span>Nome</span>:${data.name}</li>
       <li><span>Espécie</span>:${data.species}</li>
-      <li><span>Status</span>:${data.status}</li>
+      <li><span>Condição</span>:${traduzirCondicao(data)}</li>
       </ul>`;
     });
 }
@@ -38,11 +47,12 @@ function segundoPersonagem() {
   })
     .then((response) => response.json())
     .then((data) => {
-      segundoP.innerHTML = ` <img src="${data.image}" alt="${data.name}">
+      segundoP.innerHTML = `
+       <img src="${data.image}" alt="${data.name}">
       <ul>
       <li><span>Nome</span>:${data.name}</li>
       <li><span>Espécie</span>:${data.species}</li>
-      <li><span>Status</span>:${data.status}</li>
+      <li><span>Condição</span>:${traduzirCondicao(data)}</li>
       </ul>`;
     });
 }
@@ -56,11 +66,12 @@ function terceiroPersonagem() {
   })
     .then((response) => response.json())
     .then((data) => {
-      terceiroP.innerHTML = ` <img src="${data.image}" alt="${data.name}">
+      terceiroP.innerHTML = `
+     <img src="${data.image}" alt="${data.name}">
       <ul>
       <li><span>Nome</span>:${data.name}</li>
       <li><span>Espécie</span>:${data.species}</li>
-      <li><span>Status</span>:${data.status}</li>
+      <li><span>Condição</span>:${traduzirCondicao(data)}</li>
       </ul>`;
     });
 }
