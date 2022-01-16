cf11b4da11afe5bfca5954150e3d8e670b7e3786
unknown <joaosantos.js234@gmail.com>
2022-01-16 13:47:13 -0300

    finalizado

--- Readme.md
+++ Readme.md
@@ -1,8 +1,8 @@
 #nome do projeto:
-Rick and morty-api
+Rick and morty-api 
 
 #descrição:
     Como pedido foi adicionado os 3 personagens de forma aleatória
-com imagem com alt, nome, espécie e status. Como são apenas 3 personagem optei por não usar funções mais avançadas do javascript
+com imagem com alt, nome, espécie e status. 
 achei interessante adicionar um faicon(icone) antes do title e mais alguns
 detalhes na página
--- script.js
+++ script.js
@@ -1,84 +1,45 @@
 const btn = document.querySelector("#btn");
-const primeiroP = document.querySelector("#primeiroPersonagem");
-const segundoP = document.querySelector("#segundoPersonagem");
-const terceiroP = document.querySelector("#terceiroPersonagem");
+const personagens = document.querySelector("#personagens");
 
 traduzirCondicao = (data) => {
-    if(data.status == 'unknown'){
-        return 'Não sabemos';
-    }else if(data.status == 'Alive'){
-        return 'Vivo';
-    }else {
-        return 'Morto';
-    }
-}
+  if (data.status == "unknown") {
+    return "Não sabemos";
+  } else if (data.status == "Alive") {
+    return "Vivo";
+  } else {
+    return "Morto";
+  }
+};
 gerarValorAleatorio = () => {
   return Math.floor(Math.random() * 826);
 };
 
-primeiroPersonagem=()=> {
-  let numeroAleatorio = gerarValorAleatorio();
-  return fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorio}`, {
-    method: "GET",
-    headers: {
-      Accept: "application/json",
-    },
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      primeiroP.innerHTML = `
-      <img src="${data.image}" alt="${data.name}">
-      <ul>
+personagem = () => {
+  const recebe = [];
+  for (var i = 0; i < 3; i++) {
+    let numeroAleatorio = gerarValorAleatorio();
+    recebe.push(
+      fetch(
+        `https://rickandmortyapi.com/api/character/${numeroAleatorio}`
+      ).then((response) => response.json())
+    );
+
+    Promise.all(recebe).then((data) => {
+      const lista = data.reduce((acumulador, data) => {
+        acumulador += 
+      `<ul>
+      <li><img src="${data.image}" alt="${data.name}"></li>
       <li><span>Nome</span>:${data.name}</li>
       <li><span>Espécie</span>:${data.species}</li>
       <li><span>Condição</span>:${traduzirCondicao(data)}</li>
       </ul>`;
+        return acumulador;
+      }, "");
+      personagens.innerHTML = lista;
     });
-}
-
-
-function segundoPersonagem() {
-  let numeroAleatorio = gerarValorAleatorio();
-  return fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorio}`, {
-    method: "GET",
-    headers: {
-      Accept: "application/json",
-    },
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      segundoP.innerHTML = `
-       <img src="${data.image}" alt="${data.name}">
-      <ul>
-      <li><span>Nome</span>:${data.name}</li>
-      <li><span>Espécie</span>:${data.species}</li>
-      <li><span>Condição</span>:${traduzirCondicao(data)}</li>
-      </ul>`;
-    });
-}
-function terceiroPersonagem() {
-  let numeroAleatorio = gerarValorAleatorio();
-  return fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorio}`, {
-    method: "GET",
-    headers: {
-      Accept: "application/json",
-    },
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      terceiroP.innerHTML = `
-     <img src="${data.image}" alt="${data.name}">
-      <ul>
-      <li><span>Nome</span>:${data.name}</li>
-      <li><span>Espécie</span>:${data.species}</li>
-      <li><span>Condição</span>:${traduzirCondicao(data)}</li>
-      </ul>`;
-    });
-}
-
+  };
+};
 
 btn.addEventListener("click", () => {
-  primeiroPersonagem();
-  segundoPersonagem();
-  terceiroPersonagem();
+  personagem();
 });
